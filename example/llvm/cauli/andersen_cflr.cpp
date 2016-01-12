52e1b5f1f73183f17bc4188052e2638d7f492d15
Scyptnex <scyptnex@tzeidn.com>
2016-01-12 17:21:51 +1000

    andersen beginning to add different instructions, this will be tedious

--- example/llvm/cauli/andersen_cflr.cpp
+++ example/llvm/cauli/andersen_cflr.cpp
@@ -16,22 +16,20 @@ using namespace cflr;
 
 namespace {
 
-template<typename A, typename B, unsigned AC, unsigned BC>
-
 struct CauliAA : public ModulePass, public AliasAnalysis, public InstVisitor<CauliAA, void> {
     static char ID;
     unsigned num_allocs = 0;
     int num = 0;
 
-        registrar_group<Value*,unsigned> regs;
-        relation_buffer<Value*,unsigned> buf_al;
-        relation_buffer<Value*,Value*> buf_as;
-        relation_buffer<Value*,Value*> buf_lo;
-        relation_buffer<Value*,Value*> buf_st;
-        relation_buffer<Value*,unsigned> buf_pt;
-        relation_buffer<Value*,Value*> buf_ai;
+    registrar_group<Value*,unsigned> regs;
+    relation_buffer<Value*,unsigned> buf_re;
+    relation_buffer<Value*,Value*> buf_as;
+    relation_buffer<Value*,Value*> buf_lo;
+    relation_buffer<Value*,Value*> buf_st;
+    relation_buffer<Value*,unsigned> buf_pt;
+    relation_buffer<Value*,Value*> buf_al;
 
-    CauliAA() : ModulePass(ID), regs(), buf_al(regs.select<0,1>()), buf_as(regs.select<0,0>()), buf_lo(regs.select<0,0>()), buf_st(regs.select<0,0>()), buf_pt(regs.select<0,1>()), buf_ai(regs.select<0,0>()) {}
+    CauliAA() : ModulePass(ID), regs(), buf_re(regs.select<0,1>()), buf_as(regs.select<0,0>()), buf_lo(regs.select<0,0>()), buf_st(regs.select<0,0>()), buf_pt(regs.select<0,1>()), buf_al(regs.select<0,0>()) {}
 
     ~CauliAA() {
         errs() << "TOLD " << num << "\n";
@@ -56,6 +54,22 @@ struct CauliAA : public ModulePass, public AliasAnalysis, public InstVisitor<Cau
         }
         errs() << "==" << count << "==\n";
         //errs().write_escaped(m.getName()) << '\n';
+        // import as relations
+        typedef andersen_semi_naive P;
+        P::vols_t vols = regs.volumes();
+        P::rels_t relations = {relation<P::adt_t>(1),relation<P::adt_t>(1),relation<P::adt_t>(1),relation<P::adt_t>(1),relation<P::adt_t>(1),relation<P::adt_t>(1)};
+        relations[0].import_buffer(buf_re);
+        relations[1].import_buffer(buf_as);
+        relations[2].import_buffer(buf_lo);
+        relations[3].import_buffer(buf_st);
+        relations[4].import_buffer(buf_pt);
+        relations[5].import_buffer(buf_al);
+        //solve
+        //print the relations
+        std::cerr << "__load__\n";
+        relations[2].dump(std::cerr);
+        std::cerr << "__store__\n";
+        relations[3].dump(std::cerr);
         return false;
     }
 
@@ -81,14 +95,22 @@ struct CauliAA : public ModulePass, public AliasAnalysis, public InstVisitor<Cau
     //
     // Instruction Visitor
     //
-    
+
     void visitInstruction(Instruction& inst) {
         errs() << "UNSUPPORTED: " << inst << "\n";
-        llvm_unreachable("Unsupported instruction encountered");
+        //llvm_unreachable("Unsupported instruction encountered");
     }
 
-    void visitLoadInst(LoadInst& inst){
-        errs() << "LOAD: " << inst << "\n";
+    void visitLoadInst(LoadInst& inst) {
+        Value* ptr = inst.getPointerOperand();
+        Value* val = &inst;
+        buf_lo.add(relation_buffer<Value*, Value*>::outer_type {val, ptr});
+    }
+
+    void visitStoreInst(StoreInst &Inst) {
+        auto *Ptr = Inst.getPointerOperand();
+        auto *Val = Inst.getValueOperand();
+        buf_st.add(relation_buffer<Value*, Value*>::outer_type {Ptr, Val});
     }
 };
 
