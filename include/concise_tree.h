fd80d4dfca830774974a751baa9b327c5210c827
Scyptnex <scyptnex@tzeidn.com>
2016-01-20 16:47:21 +1000

    slow progress on the concise-tree

--- include/concise_tree.h
+++ include/concise_tree.h
@@ -20,7 +20,10 @@
 #ifndef __CONCISE_TREE_H__
 #define __CONCISE_TREE_H__
 
+#include <algorithm>
 #include <array>
+#include <iostream>
+#include <iomanip>
 #include <tuple>
 #include <vector>
 
@@ -36,22 +39,67 @@ template<typename I> constexpr I cidentity_mat(){
     return cidentity_mat_help<I>(0, 0);
 }
 
-struct concise_tree{
+struct concise_tree_order{
+    typedef std::pair<ident, ident> import_t;
+    const unsigned height;
+    concise_tree_order(unsigned h) : height(h) {}
+    bool operator()(const import_t& a, import_t& b){
+        unsigned aq, bq;
+        // can be made even faster: max(leading_zero(a.first ^ b.first), leading_zero(a.second ^ b.second))
+        for(int i=height; i>=0; i--){
+            //the quadrant of a row/col pair at height h depends on wether bit [h] is set
+            aq = quadrant(a.first, a.second, i);
+            bq = quadrant(b.first, b.second, i);
+            if(aq != bq) break;
+        }
+        return aq < bq;
+    }
+    static inline constexpr unsigned quadrant(ident row, ident col, unsigned height){
+        return 2*((row>>height)&1) + ((col>>height)&1);
+    }
+};
 
-    typedef ident index_t;
-    typedef std::array<index_t, 4> node_t;
+typedef ident index_t;
+typedef std::array<index_t, 4> node_t;
+
+struct concise_tree;
+
+struct concise_iterator {
+    typedef std::pair<ident, ident> value_type;
+    const concise_tree& ct;
+    std::array<unsigned, sizeof(ident)*8> istack;
+    std::array<uint8_t, sizeof(ident)*8> qstack;
+    unsigned idx;
+    unsigned qua;
+    index_t cur;
+    value_type nxt;
+    concise_iterator(const concise_tree& c) : ct(c) {}
+
+    bool operator==(const concise_iterator& other) const {
+        return &ct == &(other.ct) && idx == other.idx && qua == other.qua && cur == other.cur;
+    }
+    bool operator!=(const concise_iterator& other) const {
+        return !(other == *this);
+    }
+};
+
+/// concise_tree, the concise storage representation (contiguous) of quadtrees
+struct concise_tree{
     
     static const unsigned index_bits = sizeof(index_t)*8;
     static const unsigned node_bits = sizeof(node_t)*8;
     static const unsigned index_sl = csqrt(index_bits);
+    static const unsigned index_height = log<2>(index_sl);
     static const unsigned node_sl = index_sl*2;
-    static const unsigned node_height = log<4>(node_bits);
+    static const unsigned node_height = log<2>(node_sl);
     static const index_t identity_index = cidentity_mat<index_t>();
+    static const index_t index_rc_mask = (1<<index_height)-1;
 
     unsigned height;
+    std::vector<concise_tree_order::import_t> import_buffer;
     std::vector<node_t> tree;
 
-    concise_tree() : height(node_height), tree(1, node_t()) {}
+    concise_tree() : height(node_height), import_buffer(), tree(1, node_t()) {}
 
     bool empty() const {
         return height == node_height && std::get<0>(tree[0]) == 0 && std::get<1>(tree[0]) == 0 && std::get<2>(tree[0]) == 0 && std::get<3>(tree[0]) == 0;
@@ -63,8 +111,90 @@ struct concise_tree{
         height = node_height;
     }
 
+    void initialise_import() {
+        //ASSERT(import_buffer.empty());
+        clear();
+    }
+    void import(ident row, ident col){
+        import_buffer.push_back({row, col});
+        height = std::max(log<2>(std::max(row, col))+1, height); // runtime log<2> is inefficient(ish)
+    }
+    void finalise_import() {
+        unsigned stack[height];
+        std::sort(import_buffer.begin(), import_buffer.end(), concise_tree_order(height));
+        for(const auto& p : import_buffer){
+            unsigned cur = 0;
+            unsigned added = 0;
+            for(unsigned h=height-1; h>=node_height; h--){
+                stack[h] = cur;
+                cur += node_next(tree[cur], concise_tree_order::quadrant(p.first, p.second, h)) + 1;
+                if(cur == tree.size()){
+                    added++;
+                    tree.push_back(node_t()); // because we add in-order, this must happen
+                }
+            }
+            tree[cur][concise_tree_order::quadrant(p.first, p.second, node_height-1)] |=
+                ((index_t)1) << (
+                ((p.first&index_mask)<<index_height)|
+                (p.second&index_mask));
+            unsigned below=0;
+            for(unsigned h=node_height; h<height; h++){
+                below = std::min(below+1, added);
+                tree[stack[h]][concise_tree_order::quadrant(p.first, p.second, h)] += below;
+            }
+        }
+        import_buffer.clear();
+    }
+
+    concise_iterator begin() const{
+        concise_iterator ret(*this);
+        return ret;
+    }
+
+    concise_iterator end() const{
+        concise_iterator ret(*this);
+        ret.idx = tree.size()-1;
+        ret.qua = 3;
+        ret.cur = 0;
+        return ret;
+    }
+
+    void dump(std::ostream& out) const {
+        for(const auto& n : tree) {
+            for(unsigned i=0; i<4; i++){
+                out << (i?",":"(") << std::setw(16) << std::setfill('0') << std::hex << n[i];
+            }
+            out << ")\n";
+        }
+    }
+
+    void dump() const {
+        dump(std::cout);
+    }
+
+    /*
+     * STATIC METHODS
+     */
+
     static constexpr node_t identity_node(){
         return node_t{identity_index, 0, 0, identity_index};
+    }
+
+    /// node_next, return the offset to the subtree at quadrant q after n
+    /// for q >= 4, return the total subtree size
+    static unsigned node_next(const node_t& n, unsigned q){
+        switch(q){
+            case 0 :
+                return 0;
+            case 1 :
+                return n[0];
+            case 2 :
+                return n[0] + n[1];
+            case 3 :
+                return n[0] + n[1] + n[2];
+            default :
+                return n[0] + n[1] + n[2] + n[3];
+        }
     }
 
     // constants used by indices
--- src/main.cpp
+++ src/main.cpp
@@ -9,106 +9,26 @@
 #include <set>
 #include <iostream>
 
-#include "adt.h"
-#include "cflr.h"
-#include "neighbourhood_map.h"
-//#include "problem.h"
-#include "relation.h"
-#include "relation_buffer.h"
-#include "semi_naive.h"
-#include "utility_templates.h"
-
-namespace cflr {
-/// ulist_printer, stream the indices to an output stream
-template<typename Ul> struct ulist_printer;
-template<> struct ulist_printer<ulist<>> {
-    static inline void print(std::ostream& os){
-        os << std::endl;
-    }
-};
-template<unsigned Cur, unsigned Nxt, unsigned...Rest> struct ulist_printer<ulist<Cur, Nxt, Rest...>> {
-    static inline void print(std::ostream& os){
-        os << Cur << ", ";
-        ulist_printer<ulist<Nxt, Rest...>>::print(os);
-    }
-};
-template<unsigned Cur> struct ulist_printer<ulist<Cur>> {
-    static inline void print(std::ostream& os){
-        os << Cur;
-        ulist_printer<ulist<>>::print(os);
-    }
-};
-} // end namespace cflt
+#include "concise_tree.h"
 
 using namespace std;
-using namespace cflr;
-using namespace cflr::rule_clauses;
-
-typedef problem<
-    label<>,
-    label<>,
-    label<>,
-    rule<clause<2>>,
-    //rule<clause<2>, clause<0>, clause<2>, clause<1>>,
-    rule<clause<0>, clause<1>, clause<1>>,
-    //rule<clause<1>, clause<0>>> prob;
-    rule<clause<2>, clause<0>, clause<2>, clause<1>>> prob;
-typedef cflr::neighbourhood_map<map<ident, set<ident>>,set<ident>> nmap;
-typedef registrar_group<int> registrars_t;
-typedef semi_naive<nmap, registrars_t::size, prob> solver_t;
 
 int main(){
-    // Init
-    registrars_t regs;
-    relation_buffer<int, int> a_buf(regs.select<0, 0>());
-    relation_buffer<int, int> b_buf(regs.select<0, 0>());
-    relation_buffer<int, int> s_buf(regs.select<0, 0>());
-
-    // Read inputs
-    a_buf.from_csv("example/running/a.csv");
-    cout << "a:" << endl;
-    a_buf.to_csv(cout);
-    b_buf.from_csv("example/running/b.csv");
-    cout << endl << "b:" << endl;
-    b_buf.to_csv(cout);
-    cout << endl << "---------------------------" << endl;
-    registrars_t::volume_t volumes = regs.volumes();
-
-    // Convert to relations
-    solver_t::relations_t rels = relation_group_initialiser<nmap, problem_labels<prob>::result>::init(volumes);
-    rels[0].import_buffer(a_buf);
-    rels[1].import_buffer(b_buf);
-    rels[0].dump(cout);
-    rels[1].dump(cout);
-    rels[2].dump(cout);
-    solver_t::solve(volumes, rels);
-
-    cout << endl << solver_t::rules_t::size << endl;
-    cout << solver_t::rules_eps_t::size << endl;
-    cout << solver_t::rules_reg_t::size << endl;
-    //- nmap eps = nmap::identity(5);
-    //- a_rel.import_buffer(a_buf);
-    //- b_rel.import_buffer(b_buf);
-    //- a_rel.dump(cout);
-    //- b_rel.dump(cout);
-    //- prob::compose_delta_rule<0>(eps);
-    //- prob::compose_delta_rule<1>(eps);
-    //- cout << prob::label_count << " : " << prob::rule_count << endl << endl;
-    //- s_rel.adts[0].union_copy(eps);
-    //- s_rel.adts[0].dump(cout);
-    //- for(unsigned i=0; i<4; i++){
-    //-     nmap tmp;
-    //-     a_rel.adts[0].compose(s_rel.adts[0], tmp);
-    //-     tmp.compose(b_rel.adts[0], s_rel.adts[0]);
-    //-     cout << endl;
-    //-     s_rel.adts[0].dump(cout);
-    //- }
-    //- s_rel.adts[0].difference(eps);
-
-    // Show result
-    rels[2].export_buffer(s_buf);
-    cout << endl << "S:" << endl;
-    s_buf.to_csv(cout);
-    cout << endl;
+    cflr::concise_tree ct;
+    ct.initialise_import();
+    ct.import(0, 2);
+    ct.import(1, 1);
+    ct.import(0, 0);
+    ct.import(1, 0);
+    ct.import(1, 0);
+    ct.import(0, 1);
+    ct.import(8, 0);
+    ct.import(0, 7);
+    ct.import(7, 0);
+    ct.import(7, 4);
+    ct.import(16, 0);
+    ct.finalise_import();
+    ct.dump();
+    return 0;
 }
 
