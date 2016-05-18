909ec58fa387f3ce426979be5cf8e6815de30e1f
Scyptnex <nic.h.misc@gmail.com>
2016-05-18 11:34:45 +1000

    major gradle refactoring

---
+++ src/test/java/cauliflower/TestAST.java
@@ -0,0 +1,15 @@
+package cauliflower;
+
+import cauliflower.cflr.Label;
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class TestAST {
+
+    @Test
+    public void LabelName(){
+        Label l = new Label(0, 0);
+        assertTrue(true);
+    }
+}
---
+++ src/test/java/cauliflower/TestParser.java
@@ -0,0 +1,4 @@
+package cauliflower;
+
+public class TestParser {
+}
